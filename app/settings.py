1213e410a4baf73825de0d78163cbb9a539fc9ac
SMMousaviSP <smmousavisp@gmail.com>
2023-01-02 20:26:50 +0100

    Correcting port of the other services

--- app/settings.py
+++ app/settings.py
@@ -4,6 +4,6 @@ PORT="5003"
 
 BASE_URL = "http://localhost"
 SERVICE_ADAPTERS = [
-    ("tinyurl", BASE_URL, "5000"),
+    ("tinyurl", BASE_URL, "5002"),
 ]
-DATA_LAYER = ("datalayer", BASE_URL, "5001")
+DATA_LAYER = ("datalayer", BASE_URL, "5000")
