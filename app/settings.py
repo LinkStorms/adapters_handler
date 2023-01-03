039ab91802b630433ffad879cdd637e28f5dab48
SMMousaviSP <smmousavisp@gmail.com>
2023-01-03 11:58:23 +0100

    Make data_layer separate again

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ PORT="5003"
 
 
 BASE_URL = "http://localhost"
-SERVICES = [
-    ("data_layer", BASE_URL, "5000"),
+SERVICE_ADAPTERS = [
     ("tinyurl", BASE_URL, "5002"),
 ]
+DATA_LAYER = ("data_layer", BASE_URL, "5000")
--- app/utilities.py
+++ app/utilities.py
@@ -1,13 +1,13 @@
 import requests
 
 from settings import (
-    SERVICES,
+    SERVICE_ADAPTERS,
     DATA_LAYER
 )
 
 
 def get_service_adapter(service_name):
-    for service_adapter in SERVICES:
+    for service_adapter in SERVICE_ADAPTERS:
         if service_adapter[0] == service_name:
             return service_adapter
     return None
@@ -20,9 +20,26 @@ def get_service_adapter_url(service_name):
     raise ValueError(f"Service adapter {service_name} not found.")
 
 
-def is_service_reachable(service_name):
+def get_data_layer_url():
+    return f"{DATA_LAYER[1]}:{DATA_LAYER[2]}"
+
+
+def check_reachability(service_name, service_url):
     try:
-        response = requests.get({get_service_adapter_url(service_name)})
+        response = requests.get(service_url)
     except requests.exceptions.ConnectionError as e:
         raise Exception(f"Service {service_name} is not reachable.\nError: {e}")
     return True
+
+
+def is_service_reachable(service_name):
+    service_adapter = get_service_adapter(service_name)
+    if service_adapter:
+        service_url = get_service_adapter_url(service_name)
+        return check_reachability(service_name, service_url)
+    raise ValueError(f"Service adapter {service_name} not found.")
+
+
+def is_data_layer_reachable():
+    data_layer_url = get_data_layer_url()
+    return check_reachability("data_layer", data_layer_url)
