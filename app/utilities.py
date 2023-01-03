6a2d5e80fd0dc6a860d89e2f1a8801dfa2345024
SMMousaviSP <smmousavisp@gmail.com>
2023-01-03 12:09:03 +0100

    Add a function to return list of service adapters

--- app/utilities.py
+++ app/utilities.py
@@ -1,3 +1,5 @@
+import copy
+
 import requests
 
 from settings import (
@@ -13,8 +15,7 @@ def get_service_adapter(service_name):
     return None
 
 
-def get_service_adapter_url(service_name):
-    service_adapter = get_service_adapter(service_name)
+def get_service_adapter_url(service_adapter):
     if service_adapter:
         return f"{service_adapter[1]}:{service_adapter[2]}"
     raise ValueError(f"Service adapter {service_name} not found.")
@@ -22,6 +23,19 @@ def get_service_adapter_url(service_name):
 
 def get_data_layer_url():
     return f"{DATA_LAYER[1]}:{DATA_LAYER[2]}"
+
+
+def get_service_adapter_url_list(preferred_service_name=None):
+    if not preferred_service_name:
+        preferred_service = get_service_adapter(preferred_service_name)
+    service_adapters = copy.deepcopy(SERVICE_ADAPTERS)
+    try:
+        service_adapters.remove(preferred_service)
+        service_adapters.insert(0, preferred_service)
+    except ValueError:
+        pass
+    return [get_service_adapter_url(service_adapter) for service_adapter in service_adapters]
+        
 
 
 def check_reachability(service_name, service_url):
