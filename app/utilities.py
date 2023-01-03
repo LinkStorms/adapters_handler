ee9968b56c8d668459d8c431e2f42556ae28829f
SMMousaviSP <smmousavisp@gmail.com>
2023-01-03 12:15:58 +0100

    Fix some bugs in utilities.py

--- app/utilities.py
+++ app/utilities.py
@@ -12,13 +12,13 @@ def get_service_adapter(service_name):
     for service_adapter in SERVICE_ADAPTERS:
         if service_adapter[0] == service_name:
             return service_adapter
-    return None
+    raise ValueError(f"Service adapter {service_name} not found.")
 
 
 def get_service_adapter_url(service_adapter):
     if service_adapter:
         return f"{service_adapter[1]}:{service_adapter[2]}"
-    raise ValueError(f"Service adapter {service_name} not found.")
+    raise ValueError(f"Service adapter {service_adapter[0]} not found.")
 
 
 def get_data_layer_url():
@@ -27,7 +27,8 @@ def get_data_layer_url():
 
 def get_service_adapter_url_list(preferred_service_name=None):
     if not preferred_service_name:
-        preferred_service = get_service_adapter(preferred_service_name)
+        return [get_service_adapter_url(service_adapter) for service_adapter in SERVICE_ADAPTERS]
+    preferred_service = get_service_adapter(preferred_service_name)
     service_adapters = copy.deepcopy(SERVICE_ADAPTERS)
     try:
         service_adapters.remove(preferred_service)
