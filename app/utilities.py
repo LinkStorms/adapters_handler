ee7a8a62e39aebc9755db8efcfe7247c72660692
SMMousaviSP <smmousavisp@gmail.com>
2023-01-03 10:51:50 +0100

    Add utilities.py for getting the url of services

---
+++ app/utilities.py
@@ -0,0 +1,27 @@
+from settings import (
+    SERVICE_ADAPTERS,
+    DATA_LAYER
+)
+
+
+def get_service_adapter(service_name):
+    for service_adapter in SERVICE_ADAPTERS:
+        if service_adapter[0] == service_name:
+            return service_adapter
+    return None
+
+
+def get_service_adapter_url(service_name):
+    service_adapter = get_service_adapter(service_name)
+    if service_adapter:
+        return f"{service_adapter[1]}:{service_adapter[2]}"
+    raise ValueError(f"Service adapter {service_name} not found.")
+
+
+def get_data_layer():
+    return DATA_LAYER
+
+
+def get_data_layer_url():
+    data_layer = get_data_layer()
+    return f"{data_layer[1]}:{data_layer[2]}"
