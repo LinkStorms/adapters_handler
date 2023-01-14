9107cd22e1165768a2d3b5b79eb60ad087e91c95
ElifRuveyha <ruveyhaozmen@gmail.com>
2023-01-14 18:01:12 +0100

    Add service adapters to settings.py

--- app/settings.py
+++ app/settings.py
@@ -5,5 +5,7 @@ PORT="5003"
 BASE_URL = "http://localhost"
 SERVICE_ADAPTERS = [
     ("tinyurl", BASE_URL, "5002"),
+    ("shortenrest", BASE_URL, "5006"),
+    ("rebrandly", BASE_URL, "5004"),
 ]
 DATA_LAYER = ("data_layer", BASE_URL, "5000")
