3980573e08b04b896d246106e4e98f315bcce071
SMMousaviSP <smmousavisp@gmail.com>
2023-01-16 16:32:51 +0100

    Change the order of the service adapters

--- app/settings.py
+++ app/settings.py
@@ -4,8 +4,8 @@ PORT="5003"
 
 BASE_URL = "http://localhost"
 SERVICE_ADAPTERS = [
-    ("tinyurl", BASE_URL, "5002"),
     ("shortenrest", BASE_URL, "5006"),
     ("rebrandly", BASE_URL, "5004"),
+    ("tinyurl", BASE_URL, "5002"),
 ]
 DATA_LAYER = ("data_layer", BASE_URL, "5000")
